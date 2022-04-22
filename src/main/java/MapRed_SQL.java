fd22fe213884163a928d94d6e3856b7a62b5d3e4
christoschr97 <christoschristodoulou3@gmail.com>
2022-04-22 17:42:20 +0300

    fixing a typo

--- src/main/java/MapRed_SQL.java
+++ src/main/java/MapRed_SQL.java
@@ -4,7 +4,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Job;
-import org.a.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
