37b5b2300d4f4c6ff4d20d6872373f31372372c0
christoschr97 <christoschristodoulou3@gmail.com>
2022-04-22 17:35:09 +0300

    updated code

--- src/main/java/MapRed_SQL.java
+++ src/main/java/MapRed_SQL.java
@@ -4,7 +4,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
+import org.a.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -17,12 +17,11 @@ public class MapRed_SQL {
     Job job = Job.getInstance(conf, "mapred sql output");
     job.setJarByClass(MapRed_SQL.class);
     job.setMapperClass(Map.class);
-//    job.setCombinerClass(Reduce.class);
     job.setReducerClass(Reduce.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(MapWritable.class);
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    FileInputFormat.addInputPath(job, new Path(args[1]));
+    FileOutputFormat.setOutputPath(job, new Path(args[2]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 
@@ -37,10 +36,16 @@ public class MapRed_SQL {
 
       cust_key.set(list_of_tokens[1]);
 
-      result.put(new Text("orderKey") , new Text(list_of_tokens[0]));
-      result.put(new Text("total_price") , new Text(list_of_tokens[3]));
+      //split list_of_tokens[4] into list of tokens using the char '-'
+      String[] list_of_items = list_of_tokens[4].split("-");
 
-      context.write(cust_key, result);
+      //parse list_of_items[0] into integer and check if is between "1992" and "1998"
+      int year = Integer.parseInt(list_of_items[0]);
+      if (year >= 1992 && year <= 1998) {
+        result.put(new Text("orderKey") , new Text(list_of_tokens[0]));
+        result.put(new Text("total_price") , new Text(list_of_tokens[3]));
+        context.write(cust_key, result);
+      }
     }
   }
 
